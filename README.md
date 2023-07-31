0dbca60eeb57712c1cbbc175c98bbac101509de0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-31 12:58:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# db-teamcity-maven
+ TeamCity Maven Demo
---
+++ src/main/java/com/db/demo/math/Calc.java
@@ -0,0 +1,20 @@
+package com.db.demo.math;
+
+public class Calc {
+
+	public int addNums(int i, int j) {
+		return i + j;
+	}
+
+	public int subNums(int i, int j) {
+		return i - j;
+	}
+
+	public int divNums(int i, int j) {
+		return i / j;
+	}
+
+}
+
+
+//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
